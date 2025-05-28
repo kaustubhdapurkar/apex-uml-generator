8ec8719b6951ad1a57a41822b9c391dc4e1a2927
Kaustubh Dapurkar <kaustubhdapurkar@gmail.com>
2025-05-28 22:16:30 +0530

    fix some config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  pages: {
+    index: {
+      entry: path.resolve(__dirname, 'src/main.ts')
+    }
+  }
 })
