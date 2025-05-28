5e0b4e3919ec36111a2745578a4f051e08d43a1f
Kaustubh Dapurkar <kaustubhdapurkar@gmail.com>
2025-05-28 22:16:44 +0530

    add ts files

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import LoginView from '../views/Login.vue';
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        redirect: '/login',
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginView,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const isAuthenticated = localStorage.getItem('token');
+    
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isAuthenticated) {
+            next('/login');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+export default router; 
