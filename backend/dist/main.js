43872b7d224eccd496b88d7d51f30da4b51c8b0e
Kaustubh Dapurkar <kaustubhdapurkar@gmail.com>
2025-05-31 00:22:10 +0530

    setup cors and global path prefix

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,6 +4,14 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.setGlobalPrefix('api');
+    if (process.env.NODE_ENV !== 'production') {
+        app.enableCors({
+            origin: 'http://localhost:8080',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+            credentials: true,
+        });
+    }
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
