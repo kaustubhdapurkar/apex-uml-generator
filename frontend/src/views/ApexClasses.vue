7b6c986fe49c9a3c83e2bfc0da2db61432f6d793
Kaustubh Dapurkar <kaustubhdapurkar@gmail.com>
2025-06-22 14:29:55 +0530

    hardcode cors origin uri

---
+++ frontend/src/views/ApexClasses.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="apex-classes">
+        <div class="container">
+            <h1 class="title">Apex Classes</h1>
+            <ApexClassSearch />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ApexClassSearch from '@/components/ApexClassSearch.vue';
+</script>
+
+<style lang="scss" scoped>
+.apex-classes {
+    padding: 2rem;
+}
+
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.title {
+    margin-bottom: 2rem;
+    color: var(--text-color);
+}
+</style>
